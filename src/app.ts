e5b9a50cfd357cba5d8cf2a7626a4053784d8801
gv4383 <gregory.vargas18@gmail.com>
2021-03-12 09:14:12 -0600

    Add linting

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Application } from 'express';
-import cors from "cors" 
+import cors from 'cors';
 
 const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 app.listen(5000, (): void => console.log('Listening on port 5000'));
