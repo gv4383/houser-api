ed9bda3673401084483d52cd9f1e069184cf0fe6
gv4383 <gregory.vargas18@gmail.com>
2021-04-10 17:38:18 -0500

    Add delete house route

--- src/controllers/housesController.ts
+++ src/controllers/housesController.ts
@@ -5,7 +5,7 @@ import { House } from '../types/houses';
 
 let idCount = 2;
 
-export const getHouses = (req: Request, res: Response): Response => res.json(houses);
+export const getHouses = (_: Request, res: Response): Response => res.json(houses);
 
 export const getHouse = (req: Request, res: Response): Response => {
   const { id } = req.params;
@@ -13,10 +13,11 @@ export const getHouse = (req: Request, res: Response): Response => {
 
   if (houseIndex < 0) {
     res.status(404);
+
     return res.json({ message: 'Not Found. The requested id does not exist.' });
-  } else {
-    return res.json(houses[houseIndex]);
   }
+
+  return res.json(houses[houseIndex]);
 };
 
 export const addHouse = (req: Request, res: Response): Response => {
@@ -28,4 +29,19 @@ export const addHouse = (req: Request, res: Response): Response => {
   idCount += 1;
 
   return res.json(newHouse);
+};
+
+export const deleteHouse = (req: Request, res: Response): Response => {
+  const { id } = req.params;
+  const houseIndex = houses.findIndex((house: House) => house.id === parseInt(id));
+
+  if (houseIndex < 0) {
+    res.status(404);
+
+    return res.json({ message: 'Not Found. The requested id does not exist.' });
+  }
+
+  houses.splice(houseIndex, 1);
+
+  return res.sendStatus(204);
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { addHouse, getHouse, getHouses } from '../controllers/housesController';
+import { addHouse, deleteHouse, getHouse, getHouses } from '../controllers/housesController';
 import { getStatus } from '../controllers/statusController';
 
 const router = Router();
@@ -10,5 +10,6 @@ router.get('/status', getStatus);
 router.get('/houses', getHouses);
 router.post('/houses', addHouse);
 router.get('/houses/:id', getHouse);
+router.delete('/houses/:id', deleteHouse);
 
 export default router;
